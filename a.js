6a0204413c106723629778f221e495b91345a38a
changyiyi <changyyiyi@meituan.com>
2019-10-25 16:18:30 +0800

    Initial commit

--- a.js
+++ a.js
@@ -1,26 +1,13 @@
-register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+// sw.js 文件
+
+// 安装
+self.addEventListener('install', function (e) {
+  // 缓存 App Shell 等关键静态资源和 html (保证能缓存的内容能在离线状态跑起来)
+  console.log('install')
+});
+
+// 激活
+self.addEventListener('activate', function (e) {
+  // 激活的状态，这里就做一做老的缓存的清理工作
+  console.log('actived')
+});
